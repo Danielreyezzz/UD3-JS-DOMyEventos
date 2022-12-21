a350e28bccb278f6f77ff1374d4b358b76d33f2e
Danielreyezzz <dangioparefape@gmail.com>
2022-12-21 13:18:10 +0100

    Por fin el 018

--- 018/018orderTable.js
+++ 018/018orderTable.js
@@ -1,32 +1,14 @@
 "use strict";
+//Primero voy a necesitar  el tbody
+let tabla = document.getElementsByTagName("tbody")[0];
+//Despues almaceno las filas del tbody
+let rows = document.getElementsByTagName("tbody")[0].rows;
+//Necesito construir un array para poder ordenarlo (es un objeto de elementos tableRows)
+let rowsArray = Array.from(rows);
+//Pasamos a ordenarlo con sort. Compararemos el contenido textual del primer elemento hijo de 'a' con 'b', es decir el primer 'td'
+//Importante utilizar localCompare para poder comparar textos
+let ordenado = rowsArray.sort((a, b) => a.firstElementChild.textContent.localeCompare(b.firstElementChild.textContent))
+//Por último insertamos el tbody ordenado. Importante los '...' en el append
+tabla.append(...ordenado);
 
-let tr = document.getElementsByTagName("table");
-
-for (let i = 0; i < tr.length; i++) {
-    alert(tr[i].closest.textContent);
-    
-}
-
-alert(tr.rows)
-
-// let sorted = [];
-
-// for (let i = 0; i < tr.length; i++) {
-//     sorted = tr.sort((a,b) => a.childNodes[0].textContent.localCompare(b.childNodes[0].textContent));
-    
-// }
-
-
-
-// for (let i = 0; i < sorted.length; i++) {
-//     alert(sorted[i]);
-    
-// }
-
-// for (let i = 0; i < tr.length; i++) {
-
-//         tr[i].innerHTML = sorted[i];
-   
-    
-// }
 
